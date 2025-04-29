481041b90c62f8371c85e0f10e995f22b713324a
Oblonczyk <vinycius525@gmail.com>
2025-04-28 21:54:11 -0300

    bancos de dados

--- gerador_de_dados/conversor_csv.py
+++ gerador_de_dados/conversor_csv.py
@@ -13,11 +13,21 @@ conexao = mysql.connector.connect(
 consulta = "SELECT * FROM alunos_detalhes;"
 df = pd.read_sql(consulta, conexao)
 
+consulta_frequencia = "SELECT * FROM frequencia;"
+df_frequencia = pd.read_sql(consulta_frequencia, conexao)
+
+consulta_notas = "SELECT * FROM notas;"
+df_notas = pd.read_sql(consulta_notas, conexao)
+
 # Verificar as primeiras linhas para garantir que os dados estão corretos
 print(df.head())
+print(df_frequencia.head())
+print(df_notas.head())
 
 # Exportar para CSV com separação de colunas
 df.to_csv("alunos_detalhes.csv", index=False, encoding="utf-8", sep=";")
+df_frequencia.to_csv("frequencia_detalhes.csv", index=False, encoding="utf-8", sep=";")
+df_notas.to_csv("notas_detalhes.csv", index=False, encoding="utf-8", sep=";")
 
 # Fechar conexão
 conexao.close()
